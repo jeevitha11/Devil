593a0a7d0779f0ab0142bd422385218d7aee6bf4
jeevitha11 <jeevithanaren1@gmail.com>
2021-02-14 00:14:37 +0530

    first commit

---
+++ TRAIL/src/Demo1.java
@@ -0,0 +1,15 @@
+
+public class Demo1 {
+
+	public static void main(String[] args)
+	{
+		int n=10;
+		int res=add(n,15);
+		System.out.print(res);
+	}
+	public static int add(int n,int k)
+	{
+		int res=n+k;
+		return res;
+	}
+}
